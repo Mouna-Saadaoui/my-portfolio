0a527c767796df37c0489a64bdaab474c7b6db4b
Mouna Saadawi <saadaouimouna@tbs.u-tunis.tn>
2025-04-11 14:13:17 +0100

    khw

--- src/Home_Sections/Resume/Resume.js
+++ src/Home_Sections/Resume/Resume.js
@@ -3,10 +3,6 @@ import { motion } from 'framer-motion';
 import './Resume.css';
 
 function Resume() {
-  const handleDownloadError = () => {
-    console.error("Failed to download resume.");
-  };
-
   return (
     <section className="resume-section" id="resume">
       <motion.h2
@@ -20,19 +16,18 @@ function Resume() {
       </motion.h2>
 
       <motion.a
-        href="/ResumeMounaSaadaoui.pdf"
+        href={`${process.env.PUBLIC_URL}/ResumeMounaSaadaoui.pdf`}
         className="resume-btn"
         aria-label="Download Mouna Saadaoui's Resume"
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
         target="_blank"
         rel="noopener noreferrer"
-        download="ResumeMounaSaadaoui.pdf"
+        download
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
         viewport={{ once: true }}
-        onError={handleDownloadError}
       >
         Download My Resume
       </motion.a>
@@ -41,4 +36,5 @@ function Resume() {
 }
 
 export default Resume;
+
 
